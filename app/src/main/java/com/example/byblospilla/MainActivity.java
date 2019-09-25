43ee18cfac6e1cc0a8f506b9c6cd893d48626d88
Ventu9797 <andrea97venturelli@gmail.com>
2019-09-25 18:47:38 +0200

    pagina 1 creata yeeeeeeeeee

--- app/src/main/java/com/example/byblospilla/MainActivity.java
+++ app/src/main/java/com/example/byblospilla/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.byblospilla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,7 +21,14 @@ public class MainActivity extends AppCompatActivity {
         /*setcontentview mi mostra la prima pagina dell'applicazione!
         * R.layout.activity_main è il percorso per trovare l'interfaccia grafica
         * in generale è R.CARTELLA.NOMERISORSA */
-
+        Button btnaccess = (Button) findViewById(R.id.accessbutton);
+        final Intent openMain2Activity = new Intent(MainActivity.this, Main2Activity.class);
+        btnaccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(openMain2Activity);
+            }
+        });
     }
 
     /*qui posso scrivere gli altri metodi fuori dal metodo principale!*/
