bb2f9d37a12eb7e0bff6188e521946504cc3ccfb
dakota23 <danielakoreta@gmail.com>
2019-09-26 16:39:24 +0200

    page1 insta

--- app/src/main/java/com/example/byblospilla/Page1.java
+++ app/src/main/java/com/example/byblospilla/Page1.java
@@ -3,6 +3,9 @@ package com.example.byblospilla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class Page1 extends AppCompatActivity {
 
@@ -10,7 +13,11 @@ public class Page1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
+
+        TextView instagram = (TextView) findViewById(R.id.ig);
+        instagram.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
-    
+
 
 }
