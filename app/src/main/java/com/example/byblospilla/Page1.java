01dd24750a87a3fecf54bd86bd645c1180fd24a8
Ventu9797 <andrea97venturelli@gmail.com>
2019-09-26 18:56:24 +0200

    creata page_settings

--- app/src/main/java/com/example/byblospilla/Page1.java
+++ app/src/main/java/com/example/byblospilla/Page1.java
@@ -2,9 +2,12 @@ package com.example.byblospilla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Page1 extends AppCompatActivity {
@@ -18,6 +21,16 @@ public class Page1 extends AppCompatActivity {
         TextView instagram = (TextView) findViewById(R.id.ig);
         instagram.setMovementMethod(LinkMovementMethod.getInstance());
 
+        /*Creo collegamento al button Impostazioni*/
+        /* Accesso Admin */
+        Button btnsetting = findViewById(R.id.btn_settings);
+        final Intent openbtn_settings = new Intent(Page1.this, Page_Settings.class);
+        btnsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(openbtn_settings);
+            }
+        });
     }
 
 
