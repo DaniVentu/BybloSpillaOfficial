7788291e8752f1a264731f8a65becf16aecd7866
dakota23 <danielakoreta@gmail.com>
2019-10-01 17:56:23 +0200

    Merge branch 'master' of C:\Users\PP\AndroidStudioProjects\BybloSpillaOfficial with conflicts.

--- app/src/main/java/com/example/byblospilla/MainActivity.java
+++ app/src/main/java/com/example/byblospilla/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //imposto layout da visualizzare!!
-        /*setcontentview mi mostra la prima pagina dell'applicazione!
-        * R.layout.activity_main è il percorso per trovare l'interfaccia grafica
-        * in generale è R.CARTELLA.NOMERISORSA */
+        setContentView(R.layout.activity_main);
+
+        /*  setcontentview mi mostra la prima pagina dell'applicazione!
+            * R.layout.activity_main è il percorso per trovare l'interfaccia grafica
+            * in generale è R.CARTELLA.NOMERISORSA */
+
         Button btnaccess = (Button) findViewById(R.id.accessbutton);
         final Intent openMain2Activity = new Intent(MainActivity.this, Main2Activity.class);
         btnaccess.setOnClickListener(new View.OnClickListener() {
@@ -29,10 +33,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(openMain2Activity);
             }
         });
+
     }
 
     /*qui posso scrivere gli altri metodi fuori dal metodo principale!*/
-
-
 
 }
