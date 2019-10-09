94a9dd00b81cb6a7ce5a90b3b62e7d43a321fdf0
Ventu9797 <andrea97venturelli@gmail.com>
2019-10-09 13:05:41 +0200

    aggiunta modifica timetable
    solo bottone layout e collegamento

--- app/src/main/java/com/example/byblospilla/Page_Timetable.java
+++ app/src/main/java/com/example/byblospilla/Page_Timetable.java
@@ -50,7 +50,7 @@ public class Page_Timetable extends AppCompatActivity {
             @Override
 
             public void onClick(View view) {
-
+                /*codice per l'admin Login*/
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(Page_Timetable.this);
                 View mView = getLayoutInflater().inflate(R.layout.my_dialog, null);
                 final EditText mPassword =  mView.findViewById(R.id.numericpw);
@@ -59,17 +59,43 @@ public class Page_Timetable extends AppCompatActivity {
                 mBuilder.setView(mView);
                 final AlertDialog dialog = mBuilder.create();
                 dialog.show();
+
+                /*codice per impostare il Dialog di modifica*/
+
+                final Intent openLogin = new Intent(Page_Timetable.this,Page_modify.class);
+
                 mLogin.setOnClickListener(new View.OnClickListener() {
 
                     @Override
                     public void onClick(View view) {
                         String pw = "1923";
-                        if(!mPassword.getText().toString().isEmpty()){
-                            Toast.makeText(Page_Timetable.this, R.string.no, Toast.LENGTH_SHORT).show();
+                        if(mPassword.getText().toString().isEmpty()){
+                            Toast.makeText(Page_Timetable.this, "Inserisci codice Admin", Toast.LENGTH_SHORT).show();
                             dialog.dismiss();
                         }else if (mPassword.getText().toString().equals(pw)){
                             Toast.makeText(Page_Timetable.this, R.string.si,Toast.LENGTH_SHORT).show();
-                            startActivity(openShowDialog);
+                            dialog.dismiss();
+                            /*inizio codice modifica timetable*/
+                            AlertDialog.Builder LBuilder = new AlertDialog.Builder(Page_Timetable.this);
+                            View LView = getLayoutInflater().inflate(R.layout.modify_timetable, null);
+                            Button LSalva = LView.findViewById(R.id.btnsalva);
+                            LBuilder.setView(LView);
+                            final AlertDialog dialog1 = LBuilder.create();
+                            dialog1.show();
+
+                            LSalva.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Toast.makeText(Page_Timetable.this,"Modifiche Salvate Correttamente!",Toast.LENGTH_SHORT).show();
+                                    /*
+                                    * Istruzioni di salvataggio
+                                    * */
+                                    dialog1.dismiss();
+                                }
+                            });
+
+                            //startActivity(openShowDialog); // da cancellare
+                            /*fine codice modifica timetable*/
                         }
                         else{
                             Toast.makeText(Page_Timetable.this, R.string.no, Toast.LENGTH_SHORT).show();
