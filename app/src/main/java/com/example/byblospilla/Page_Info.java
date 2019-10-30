85b7c16bc390490dc9a4d0e2ca094cde49cb1701
dakota23 <danielakoreta@gmail.com>
2019-10-30 12:35:16 +0100

    modify

--- app/src/main/java/com/example/byblospilla/Page_Info.java
+++ app/src/main/java/com/example/byblospilla/Page_Info.java
@@ -2,10 +2,13 @@ package com.example.byblospilla;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class Page_Info extends AppCompatActivity {
 
@@ -13,6 +16,36 @@ public class Page_Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page__info);
+
+        /* Card5 Founders*/
+        CardView cardFounders = findViewById(R.id.card5);
+
+        cardFounders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertadd = new AlertDialog.Builder(Page_Info.this);
+                LayoutInflater factory = LayoutInflater.from(Page_Info.this);
+                final View view = factory.inflate(R.layout.card_founders, null);
+                alertadd.setView(view);
+                alertadd.show();
+            }
+        });
+
+        /* Card6 Founders*/
+        CardView cardDevelopers = findViewById(R.id.card6);
+
+        cardDevelopers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertadd = new AlertDialog.Builder(Page_Info.this);
+                LayoutInflater factory = LayoutInflater.from(Page_Info.this);
+                final View view = factory.inflate(R.layout.card_developers, null);
+                alertadd.setView(view);
+                alertadd.show();
+            }
+        });
+
+
 
         /* return */
         Button btnret = findViewById(R.id.retbtn);
--- app/src/main/java/com/example/byblospilla/Page_Rooms.java
+++ app/src/main/java/com/example/byblospilla/Page_Rooms.java
@@ -1,10 +1,15 @@
 package com.example.byblospilla;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -15,18 +20,33 @@ public class Page_Rooms extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page__rooms);
 
+        /* collegamento cardview Aula Gialla con foto */
 
-        /*CardView cardG = findViewById(R.id.cardGialla);
-        final Intent opencardG = new Intent (Page_Rooms.this, Page_Gallery.class);
-        cardG.setOnClickListener(new View.OnClickListener(){
-            public void onClick (View v) {
-                startActivity(opencardG);
+        CardView cardG = findViewById(R.id.cardGialla);
+
+        cardG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertadd_1 = new AlertDialog.Builder(Page_Rooms.this);
+                LayoutInflater factory_1 = LayoutInflater.from(Page_Rooms.this);
+                final View view_1 = factory_1.inflate(R.layout.card_gialla, null);
+                alertadd_1.setView(view_1);
+                alertadd_1.show();
             }
-        }); */
+        });
 
-
-
-        /* return */
+        /* collegamento cardview Aula Verde con foto */
+        CardView cardV = findViewById(R.id.cardVerde);
+        cardV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertadd_2 = new AlertDialog.Builder(Page_Rooms.this);
+                LayoutInflater factory_2 = LayoutInflater.from(Page_Rooms.this);
+                final View view_2 = factory_2.inflate(R.layout.card_verde, null);
+                alertadd_2.setView(view_2);
+                alertadd_2.show();
+            }
+        });
 
 
         Button btnret = findViewById(R.id.retbtn);
